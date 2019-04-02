b1cc6796ad2e017d4133665a6f31b4fbc613dcf4
ssrp <2014csb1029@iitrpr.ac.in>
2019-04-03 00:11:53 +0800

    bla

---
+++ audioenhancement/js/frame.js
@@ -0,0 +1,14 @@
+function setScreenSize()
+{
+	/*
+	document.body.style.zoom = "80%";
+	var size = screen.width;
+	size = size - 125;
+	document.getElementById("container").style.width = size+"px";
+	document.getElementById("container").style.marginLeft = "auto";
+	document.getElementById("container").style.marginRight = "auto";
+	document.getElementById("container").style.opacity = "1";
+	*/
+	// do something
+
+}
