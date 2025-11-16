62f7c9ab3e1dc6d8aeb089b1f98e30297a6becd1
ssrp <phaye.samarth@gmail.com>
2025-11-16 16:52:32 +0530

    add pictures

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -13,12 +13,20 @@ const introData = {
 };
 const Intro = () => {
   const [isHovering, setIsHovering] = React.useState(false);
+  const [profileImage, setProfileImage] = React.useState("");
+  
   const onMouseOver = _ => {
     setIsHovering(true);
   };
   const onMouseOut = _ => {
     setIsHovering(false);
   };
+
+  // Randomly select a profile image on component mount
+  React.useEffect(() => {
+    const randomImageNumber = Math.floor(Math.random() * 8) + 1; // Random number 1-8
+    setProfileImage(`/images/profiles/${randomImageNumber}.jpg`);
+  }, []);
 
   const copyEmailToClipboard = () => {
     navigator.clipboard.writeText(introData.email).then(() => {
@@ -45,23 +53,31 @@ const Intro = () => {
   }, []);
 
   return <header className="intro">
-    <h1 className="intro__hello">{introData.title}
-      <span className={isHovering ? "emoji wave-hand animated wave" : "emoji wave-hand animated"} onMouseOver={onMouseOver} onMouseOut={onMouseOut} onFocus={onMouseOver} onBlur={onMouseOut} role="button" tabIndex={0} aria-label="wave hand"></span>
-    </h1>
+    <div className="intro__content">
+      <div className="intro__text">
+        <h1 className="intro__hello">{introData.title}
+          <span className={isHovering ? "emoji wave-hand animated wave" : "emoji wave-hand animated"} onMouseOver={onMouseOver} onMouseOut={onMouseOut} onFocus={onMouseOver} onBlur={onMouseOut} role="button" tabIndex={0} aria-label="wave hand"></span>
+        </h1>
 
-    <h2 className="intro__tagline">{introData.beforeName}
-      <span className="name">{introData.name}</span>{introData.afterName}
-      <span className="emoji technologist"></span>
-    </h2>
+        <h2 className="intro__tagline">{introData.beforeName}
+          <span className="name">{introData.name}</span>{introData.afterName}
+          <span className="emoji technologist"></span>
+        </h2>
 
-    <h3 className="intro__contact">
-      <span>{introData.contact}</span>
-      <span className="emoji pointer"></span>
-      <span>
-      <button onClick={copyEmailToClipboard}
-         className="highlight-link email-button">{introData.email}</button>
-    </span>
-    </h3>
+        <h3 className="intro__contact">
+          <span>{introData.contact}</span>
+          <span className="emoji pointer"></span>
+          <span>
+          <button onClick={copyEmailToClipboard}
+             className="highlight-link email-button">{introData.email}</button>
+        </span>
+        </h3>
+      </div>
+      
+      <div className="intro__image">
+        <img src={profileImage} alt="Picture didn't load, a thousand words lost." className="intro__profile-pic" />
+      </div>
+    </div>
   </header>
 };
 
